766056b886b3173f4174ab4749667dd902631e7b
Prady2006 <pradyumnsharma7@gmail.com>
2020-08-17 14:29:31 +0530

    production

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('./config/environment');
 const cookieParser = require('cookie-parser');
 const app = express();
+require('./config/view-helper')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
